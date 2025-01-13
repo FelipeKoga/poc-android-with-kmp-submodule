dceb8edf801fd40055970c4cf63bc8105224e85e
koga <felipe.koga@hotmail.com>
2025-01-12 22:15:59 -0300

    chore: integrate analytics from submodule

--- app/src/main/java/dev/koga/poc_android/MainActivity.kt
+++ app/src/main/java/dev/koga/poc_android/MainActivity.kt
@@ -4,12 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import dev.koga.poc_android.ui.theme.PocandroidTheme
+import dev.koga.poc_kmp_submodule.Analytics
 import dev.koga.poc_kmp_submodule.Greeting
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +27,27 @@ class MainActivity : ComponentActivity() {
         setContent {
             PocandroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Text(
-                        text = "${Greeting().greet()}!",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
+                    ) {
+                        Text(
+                            text = "${Greeting().greet()}!",
+                            modifier = Modifier.padding(innerPadding)
+                        )
+
+                        Spacer(modifier = Modifier.height(24.dp))
+
+                        Button(onClick = {
+                            Analytics.logEvent(event = "test", params = emptyMap())
+                        }) {
+                            Text(text = "Log event to console")
+                        }
+                    }
+
                 }
             }
         }
